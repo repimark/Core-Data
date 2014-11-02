a14b80cbad882ba9200119abcd2c38a1a6605281
repimark <repitsm@gmail.com>
2014-11-02 15:53:48 +0100

    Initial Commit

---
+++ Core Data/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Data
+//
+//  Created by Márk Repits on 2014.11.02..
+//  Copyright (c) 2014 Márk Repits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core Data/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Data
+//
+//  Created by Márk Repits on 2014.11.02..
+//  Copyright (c) 2014 Márk Repits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
